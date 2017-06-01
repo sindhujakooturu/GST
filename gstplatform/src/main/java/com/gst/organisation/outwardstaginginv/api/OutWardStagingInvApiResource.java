c8c69a04829ac04b3d47e0675cee8bce8059c19f
sindhujareddy026 <sindhujareddy026@gmail.com>
2017-06-01 12:56:01 +0530

    Changes in Outward Staging Invoice

---
+++ gstplatform/src/main/java/com/gst/organisation/outwardstaginginv/api/OutWardStagingInvApiResource.java
@@ -0,0 +1,138 @@
+package com.gst.organisation.outwardstaginginv.api;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.gst.commands.domain.CommandWrapper;
+import com.gst.commands.service.CommandWrapperBuilder;
+import com.gst.commands.service.PortfolioCommandSourceWritePlatformService;
+import com.gst.infrastructure.core.api.ApiRequestParameterHelper;
+import com.gst.infrastructure.core.data.CommandProcessingResult;
+import com.gst.infrastructure.core.serialization.ApiRequestJsonSerializationSettings;
+import com.gst.infrastructure.core.serialization.DefaultToApiJsonSerializer;
+import com.gst.infrastructure.security.service.PlatformSecurityContext;
+import com.gst.organisation.outwardstaginginv.data.OutWardStagingInvData;
+import com.gst.organisation.outwardstaginginv.service.OutWardStagingInvReadPlatformService;
+
+/**
+ * @author 
+ * 
+ */
+@Path("/outwardinv")
+@Component
+@Scope("singleton")
+public class OutWardStagingInvApiResource {
+
+
+	private final Set<String> RESPONSE_PARAMETERS = new HashSet<String>(Arrays.asList("id", "gstin", "gstinPurchaser","cName",
+			"supplierInvNo", "supplierInvDate","supplierInvValue", "supplyStateCode","orderNo","orderDate","etin","invoiceId","receiptStateCode",
+			"status","errorCode","errorDescripter"));
+	
+	private final String resourceNameForPermissions = "OUTWARDINV";
+	private final PlatformSecurityContext context;
+	private final PortfolioCommandSourceWritePlatformService commandSourceWritePlatformService;
+	private final DefaultToApiJsonSerializer<OutWardStagingInvData> toApiJsonSerializer;
+	private final ApiRequestParameterHelper apiRequestParameterHelper;
+	private final OutWardStagingInvReadPlatformService outWardStagingInvReadPlatformService;
+
+	@Autowired
+	public OutWardStagingInvApiResource(final PlatformSecurityContext context,final PortfolioCommandSourceWritePlatformService commandSourceWritePlatformService,
+			final DefaultToApiJsonSerializer<OutWardStagingInvData> toApiJsonSerializer,final ApiRequestParameterHelper apiRequestParameterHelper,
+			final OutWardStagingInvReadPlatformService outWardStagingInvReadPlatformService) {
+		
+		this.context = context;
+		this.commandSourceWritePlatformService = commandSourceWritePlatformService;
+		this.toApiJsonSerializer = toApiJsonSerializer;
+		this.apiRequestParameterHelper = apiRequestParameterHelper;
+		this.outWardStagingInvReadPlatformService = outWardStagingInvReadPlatformService;
+	}
+
+	/**
+	 * @param uriInfo
+	 * @return retrieved list of all outwardinv details
+	 */
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String retrieveAllOutWardInvData(@Context final UriInfo uriInfo) {
+		
+	    context.authenticatedUser().validateHasReadPermission(resourceNameForPermissions);
+		final List<OutWardStagingInvData> outWardStagingInvData = this.outWardStagingInvReadPlatformService.retrieveAllOutWardInvData();
+		final ApiRequestJsonSerializationSettings settings = apiRequestParameterHelper.process(uriInfo.getQueryParameters());
+		return this.toApiJsonSerializer.serialize(settings, outWardStagingInvData,RESPONSE_PARAMETERS);
+
+	}
+
+	/**
+	 * @param uriInfo
+	 * @param apiRequestBodyAsJson
+	 * @return
+	 */
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+
+	public String createOutWardInvData(final String apiRequestBodyAsJson,@Context final UriInfo uriInfo) {
+		
+		context.authenticatedUser().validateHasReadPermission(resourceNameForPermissions);
+		final CommandWrapper commandRequest = new CommandWrapperBuilder().createOutWardInv().withJson(apiRequestBodyAsJson).build();
+		final CommandProcessingResult result = this.commandSourceWritePlatformService.logCommandSource(commandRequest);
+		return this.toApiJsonSerializer.serialize(result);
+	
+	}
+
+	/**
+	 * @param outWardInvId
+	 * @param uriInfo
+	 * @return retrieved single outWardInvId details
+	 */
+	@GET
+	@Path("{outWardInvId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+
+	public String retrieveSingleOutWardInvDataDetails(@PathParam("outWardInvId") final Long outWardInvId,@Context final UriInfo uriInfo) {
+	   
+		context.authenticatedUser().validateHasReadPermission(resourceNameForPermissions);
+		OutWardStagingInvData outWardStagingInvData = this.outWardStagingInvReadPlatformService.retrieveSingleOutWardStagingInvDetails(outWardInvId);
+		final ApiRequestJsonSerializationSettings settings = apiRequestParameterHelper.process(uriInfo.getQueryParameters());
+		return this.toApiJsonSerializer.serialize(settings,outWardStagingInvData,RESPONSE_PARAMETERS);
+	}
+
+	/**
+	 * @param outWardInvId
+	 * @param apiRequestBodyAsJson
+	 * @return update outWardInvId here
+	 */
+	@PUT
+	@Path("{outWardInvId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+
+	public String updateSingleOutWardInvData(@PathParam("outWardInvId") final Long outWardInvId,final String apiRequestBodyAsJson) {
+	   
+		context.authenticatedUser().validateHasReadPermission(resourceNameForPermissions);
+		final CommandWrapper commandRequest = new CommandWrapperBuilder().updateOutWardInv(outWardInvId).withJson(apiRequestBodyAsJson)	.build();
+		final CommandProcessingResult result = this.commandSourceWritePlatformService.logCommandSource(commandRequest);
+		return this.toApiJsonSerializer.serialize(result);
+	}
+	
+
+}
---
+++ gstplatform/src/main/java/com/gst/organisation/outwardstaginginv/serialization/OutWardStagingInvCommandFromApiJsonDeserializer.java
@@ -0,0 +1,121 @@
+package com.gst.organisation.outwardstaginginv.serialization;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.JsonElement;
+import com.gst.infrastructure.core.data.ApiParameterError;
+import com.gst.infrastructure.core.data.DataValidatorBuilder;
+import com.gst.infrastructure.core.exception.InvalidJsonException;
+import com.gst.infrastructure.core.exception.PlatformApiDataValidationException;
+import com.gst.infrastructure.core.serialization.FromJsonHelper;
+
+/**
+ * @author Deserializer for code JSON to validate API request.
+ */
+@Component
+public class OutWardStagingInvCommandFromApiJsonDeserializer {
+
+	/**
+	 * The parameters supported for this command.
+	 */
+	final private Set<String> supportedParameters = new HashSet<String>(Arrays.asList("gstin","gstinPurchaser","cName","supplierInvNo",
+			          "supplierInvDate", "supplierInvValue", "supplyStateCode","orderNo" , "orderDate","etin","invoiceId",
+			          "receiptStateCode", "status", "errorCode", "errorDescripter","dateFormat", "locale"));
+	
+	private final FromJsonHelper fromApiJsonHelper;
+
+	@Autowired
+	public OutWardStagingInvCommandFromApiJsonDeserializer(final FromJsonHelper fromApiJsonHelper) {
+		this.fromApiJsonHelper = fromApiJsonHelper;
+	}
+
+	/**
+	 * @param json
+	 * check validation for create OutWardStagingInv codes
+	 */
+
+	public void validaForCreate(String json) {
+		if (StringUtils.isBlank(json)) {
+			throw new InvalidJsonException();
+		}
+
+		final Type typeOfMap = new TypeToken<Map<String, Object>>() {
+			private static final long serialVersionUID = 1L;
+		}.getType();
+
+		fromApiJsonHelper.checkForUnsupportedParameters(typeOfMap, json,supportedParameters);
+
+		final List<ApiParameterError> dataValidationErrors = new ArrayList<ApiParameterError>();
+		final DataValidatorBuilder baseDataValidator = new DataValidatorBuilder(dataValidationErrors).resource("outwardinv");
+
+		final JsonElement element = fromApiJsonHelper.parse(json);
+
+		final String gstin = fromApiJsonHelper.extractStringNamed("gstin", element);
+		baseDataValidator.reset().parameter("gstin").value(gstin);
+
+		final String gstinPurchaser = fromApiJsonHelper.extractStringNamed("gstinPurchaser", element);
+		baseDataValidator.reset().parameter("gstinPurchaser").value(gstinPurchaser);
+
+		final String cName = fromApiJsonHelper.extractStringNamed("cName", element);
+		baseDataValidator.reset().parameter("cName").value(cName);
+		
+		final Integer supplierInvNo = fromApiJsonHelper.extractIntegerWithLocaleNamed("supplierInvNo", element);
+		baseDataValidator.reset().parameter("supplierInvNo").value(supplierInvNo);
+	
+		final String supplierInvDate = fromApiJsonHelper.extractStringNamed("supplierInvDate", element);
+		baseDataValidator.reset().parameter("supplierInvDate").value(supplierInvDate);
+
+		final Long supplierInvValue = fromApiJsonHelper.extractLongNamed("supplierInvValue", element);
+		baseDataValidator.reset().parameter("supplierInvValue").value(supplierInvValue);
+		
+		final String supplyStateCode = fromApiJsonHelper.extractStringNamed("supplyStateCode", element);
+		baseDataValidator.reset().parameter("supplyStateCode").value(supplyStateCode);
+		
+		final String orderNo = fromApiJsonHelper.extractStringNamed("orderNo", element);
+		baseDataValidator.reset().parameter("orderNo").value(orderNo);
+
+		final String orderDate = fromApiJsonHelper.extractStringNamed("orderDate", element);
+		baseDataValidator.reset().parameter("orderDate").value(orderDate);
+
+		final String etin = fromApiJsonHelper.extractStringNamed("etin", element);
+		baseDataValidator.reset().parameter("etin").value(etin);
+		
+		final Long invoiceId = fromApiJsonHelper.extractLongNamed("invoiceId", element);
+		baseDataValidator.reset().parameter("invoiceId").value(invoiceId);
+	
+		final String receiptStateCode = fromApiJsonHelper.extractStringNamed("receiptStateCode", element);
+		baseDataValidator.reset().parameter("receiptStateCode").value(receiptStateCode);
+
+		final Long status = fromApiJsonHelper.extractLongNamed("status", element);
+		baseDataValidator.reset().parameter("status").value(status);
+		
+		final String errorCode = fromApiJsonHelper.extractStringNamed("errorCode", element);
+		baseDataValidator.reset().parameter("errorCode").value(errorCode);
+		
+		final String errorDescripter = fromApiJsonHelper.extractStringNamed("errorDescripter", element);
+		baseDataValidator.reset().parameter("errorDescripter").value(errorDescripter);
+
+		throwExceptionIfValidationWarningsExist(dataValidationErrors);
+
+	}
+
+	private void throwExceptionIfValidationWarningsExist(
+			final List<ApiParameterError> dataValidationErrors) {
+		if (!dataValidationErrors.isEmpty()) {
+			throw new PlatformApiDataValidationException(
+					"validation.msg.validation.errors.exist",
+					"Validation errors exist.", dataValidationErrors);
+		}
+	}
+}
